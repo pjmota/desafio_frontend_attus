84c297906f5290bb01f7ac7f9d7e53e626fcd501
Paulo Mota <paulob1@hotmail.com>
2026-04-19 14:06:03 -0300

    chore: bootstrap desafio-pratico (Angular 17 + Material + NgRx + Jest)

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+};
