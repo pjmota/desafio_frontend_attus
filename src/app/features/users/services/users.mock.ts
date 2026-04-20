17fbdbf9d397b4d7c993ec6873f4baa767e391dc
Paulo Mota <paulob1@hotmail.com>
2026-04-20 13:28:57 -0300

    refactor: isola base de dados mockada da API service

---
+++ src/app/features/users/services/users.mock.ts
@@ -0,0 +1,52 @@
+import { User } from '../models/user.model';
+
+export const USERS_MOCK: readonly User[] = [
+  {
+      id: 'u1',
+      name: 'Ana Silva',
+      email: 'ana.silva@empresa.com',
+      cpf: '12345678901',
+      phone: '11999990000',
+      phoneType: 'Celular',
+    },
+    {
+      id: 'u2',
+      name: 'Bruno Costa',
+      email: 'bruno.costa@empresa.com',
+      cpf: '98765432100',
+      phone: '11333330000',
+      phoneType: 'Fixo',
+    },
+    {
+      id: 'u3',
+      name: 'Carla Souza',
+      email: 'carla.souza@empresa.com',
+      cpf: '11122233344',
+      phone: '11988887777',
+      phoneType: 'Celular',
+    },
+    {
+      id: 'u4',
+      name: 'Larry S. Conner',
+      email: 'larry.conner@empresa.com',
+      cpf: '11122233344',
+      phone: '11988887777',
+      phoneType: 'Celular',
+    },
+    {
+      id: 'u5',
+      name: 'Emily Verbeke',
+      email: 'emily.verbeke@empresa.com',
+      cpf: '11122233344',
+      phone: '11988887777',
+      phoneType: 'Celular',
+    },
+    {
+      id: 'u6',
+      name: 'Maryam David',
+      email: 'carla.david@empresa.com',
+      cpf: '11122233344',
+      phone: '11988887777',
+      phoneType: 'Celular',
+    },
+];
